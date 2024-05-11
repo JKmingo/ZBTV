7ac898b319a4d4a0036d23e30bb74bfeadbad8d8
JKmingo <jimic0831@gmail.com>
2024-05-11 15:23:59 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ zb_source_limit = 10
 response_time_weight = 0.5
 resolution_weight = 0.5
 # 订阅url，在https://github.com/xisohi/IPTV-Multicast-source中找自己想要的
-subscribe_url = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/xisohi/IPTV-Multicast-source/main/shanghai/telecom.txt"
+subscribe_url = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/xisohi/IPTV-Multicast-source/main/beijing/telecom.txt"
 # 在http://tonkiang.us网站上搜索的关键词
-search_keyword = "上海电信"
+search_keyword = "北京电信"
 # ftp上传result.txt文件
 ftp_host = ""
 ftp_port = ""
