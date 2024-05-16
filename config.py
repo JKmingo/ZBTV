7ad24ebc21329d3a020d4a211dbbbe744b247110
JKmingo <jimic0831@gmail.com>
2024-05-16 13:49:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,14 @@ final_file = "result.txt"
 zb_urls_limit = 10
 response_time_weight = 0.5
 resolution_weight = 0.5
+# 1: 显示默认值，线路1 线路2，2：显示视频分辨率，如:1080x720
+xianlu_type = 2
+# ffmpeg解析视频时间，单位秒
+ffmpeg_time = 10
 # key: 地区，在http://tonkiang.us网站上搜索的关键词
 # value: 订阅url，在https://github.com/xisohi/IPTV-Multicast-source中找自己想要的
 search_dict = {
-    "北京": "https://mirror.ghproxy.com/https://raw.githubusercontent.com/xisohi/IPTV-Multicast-source/main/beijing/beijing.txt",
-    "四川": "https://mirror.ghproxy.com/https://raw.githubusercontent.com/xisohi/IPTV-Multicast-source/main/sichuan/telecom.txt",
+    "上海": "https://mirror.ghproxy.com/https://raw.githubusercontent.com/xisohi/IPTV-Multicast-source/main/shanghai/telecom.txt"
 }
 # ftp上传result.txt文件
 ftp_host = ""
