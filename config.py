e9fb796fe1ea8eadec519f4ef553be4eb12f9359
JKmingo <jimic0831@gmail.com>
2024-05-21 16:17:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ final_file = "result.txt"
 zb_urls_limit = 10
 response_time_weight = 0.5
 resolution_weight = 0.5
+open_sort = True
 # 1: 显示默认值，线路1 线路2，2：显示视频分辨率，如:1080x720
 xianlu_type = 2
 # ffmpeg解析视频时间，单位秒
@@ -13,16 +14,17 @@ search_dict = {
     "上海": "https://mirror.ghproxy.com/https://raw.githubusercontent.com/xisohi/IPTV-Multicast-source/main/shanghai/telecom.txt"
 }
 # 在http://tonkiang.us网站上搜索的源的页数
-search_page_num = 10
+search_page_num = 1
 # 忽略的关键词，比如在demo.txt中配置广东珠江,但在订阅中只有广东珠江高清,就需要忽略掉"高清"
 search_ignore_key = ["高清", "4K"]
 # crawl_type的默认值为1-只爬取http://tonkiang.us上组播源；2-只爬取crawl_urls中配置的网站；3-全部
-crawl_type = "1"
+crawl_type = "2"
 # 收集其他大佬url中的直播源
 crawl_urls = [
-    "https://fanmingming.com/txt?url=https://live.fanmingming.com/tv/m3u/ipv6.m3u",
-    "https://fanmingming.com/txt?url=https://live.fanmingming.com/tv/m3u/ipv6Plus.m3u"
+    "https://github.moeyy.xyz/https://raw.githubusercontent.com/PizazzGY/TVBox/main/live.txt"
 ]
+# ipv6源检测有效性的代理地址，用于不支持ipv6网络的主机，若网络支持ipv6，这里填空""
+ipv6_proxy = "http://www.ipv6proxy.net/go.php?u=" #此代理用于github，国内不一定能用
 # ftp上传result.txt文件
 ftp_host = ""
 ftp_port = ""
